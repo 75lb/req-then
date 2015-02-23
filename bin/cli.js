92536680d4bcaa66e218a820cf0c51dfd8034171
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-02-23 13:26:05 +0000

    cli can now send data

--- bin/cli.js
+++ bin/cli.js
@@ -5,11 +5,12 @@ var util = require("util");
 
 var url = process.argv[2];
 var method = process.argv[3];
+var data = process.argv[4];
 
 Request()
 	.on("open", function(){ console.log("OPEN\n===="); })
 	.on("end", function(){ console.log("ENDED\n====="); })
-	.connect(url, { method: method })
+	.connect(url, { method: method, data: data })
 	.then(function(res){
 		console.log("RESPONSE\n========");
 		console.log(util.inspect(res.res, { depth: 0 }));
