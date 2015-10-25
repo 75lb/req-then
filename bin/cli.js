1bec7a778a4c32dfa3f5ce346ce996770403fea2
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-10-25 13:27:11 +0000

    removed Request

--- bin/cli.js
+++ bin/cli.js
@@ -8,13 +8,13 @@ var url = process.argv[2]
 var method = process.argv[3]
 var data = process.argv[4]
 
-createRequest(url, { method: method, data: data }).promise
-  .then(function (request) {
+createRequest(url, { method: method, data: data })
+  .then(function (response) {
     console.log('RESPONSE\n========')
-    console.log(util.inspect(o.select(request.in.res, [ 'statusCode', 'statusMessage', 'headers' ]), { depth: null }))
+    console.log(util.inspect(o.select(response.res, [ 'statusCode', 'statusMessage', 'headers' ]), { depth: null }))
 
     console.log('\nDATA\n====')
-    console.log(request.in.data)
+    console.log(response.data)
   })
   .catch(function (err) {
     console.error('ERROR: ' + err.message)
